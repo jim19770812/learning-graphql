d75a195e18a7b38e23e8e5999583ebffd2eb788d
jim <xiaofeng.han@inrevo.io>
2021-08-09 22:52:34 +0800

    1

--- learning-graphql-client/src/views/chapter05-执行.vue
+++ learning-graphql-client/src/views/chapter05-执行.vue
@@ -1,6 +1,83 @@
 <template>
   <div>
     <h1>第5章 执行</h1>
+    <ul>
+      <li>执行的结果一般是json格式，也支持别的格式比如yaml格式</li>
+    </ul>
+    本章例子的定义：后端部分
+    <pre>
+    type Query{ <strong>//Query是根字段，也是所有查询的入口点</strong>
+      human(id: ID!): Human
+    }
+    type Human{
+      name: String!
+      gender: String!
+      age : Int!
+      Job : [Job!]
+    }
+    type Job{
+      name: String!
+      salary: Float
+    }
+    </pre>
+    本章例子的定义：前端查询
+    <pre>
+    {
+      human(id: 1002){ <strong>//查询id=1002的人</strong>
+        name
+        jobs{ <strong>//多层级</strong>
+          name
+        }
+      }
+    }
+    </pre>
+    上述例子的解析
+    <ul>
+      <li>
+        <div class="green">根字段&amp;解析器</div>
+        <ul>
+          <li>后端API的入口点Query，注意不同的实现语言的实现逻辑有差异
+            <pre>
+              <strong>//基于js的实现</strong>
+              Query:{
+                human(obj, args, context, info){
+                  return context.db.loadHumanById(args.id).then(userData=>new Human(userData))
+                }
+              }
+            </pre>
+            其中的参数字段的解释如下
+            <ul>
+              <li>obj：上一级对象</li>
+              <li>args：可以提供在GraphQL查询中传入的参数</li>
+              <li>context：会被提供给所有解析器，并且保存上下文访问信息</li>
+              <li>info：保存和当前查询有关的字段信息和schema详细信息的值</li>
+            </ul>
+          </li>
+          <li>返回的数据四个Human类型，可以通过这个类型约束返回的字段</li>
+        </ul>
+      </li>
+      <li>
+        <div class="green">标量强制</div>
+        标量就是基本类型，标量限制就是基本类型限制
+      </li>
+      <li>
+        <div class="green">列表解析器</div>
+        <pre>
+    {
+      human(id: 1002){
+        name
+        jobs{
+          name
+        }
+      }
+    }
+    <strong>//因为返回的jobs是个list，所以返回的数据就是个列表</strong>
+        </pre>
+      </li>
+      <li>
+        <div class="green">产生结果</div>
+      </li>
+    </ul>
   </div>
 </template>
 
--- learning-graphql-client/src/views/chapter06-内省.vue
+++ learning-graphql-client/src/views/chapter06-内省.vue
@@ -1,6 +1,74 @@
 <template>
   <div>
     <h1>第6章 内省</h1>
+    <ul>
+      <li>内省类似java的反射</li>
+      <li>通过访问__schema对象来获取GraphQL的可用类型
+        <code>
+          console.log(__schema)
+        </code>
+        <pre>
+  <strong>//所有的类型都会出现在这里</strong>
+  {
+    {"name":"Mutation"},
+    {"name":"Episode"},
+    {"name":"Human"}
+  }
+        </pre>
+      </li>
+      <li>以双下划线开头的都属于内省的一部分</li>
+    </ul>
+
+    几个关于内省的例子
+    <pre>
+    {
+      __type(name: "Human"){
+        name <strong>//类型名字</strong>
+        kind <strong>//接口/类</strong>
+        fields <strong>//包含的字段列表</strong>
+      }
+    }
+    </pre>
+
+    返回结果如下
+    <pre>
+    {"data":{
+      "_type":{
+        "name":"Human",
+        "kind":"OBJECT",<strong>//如果是接口，这里会返回INTERFACE</strong>
+        "fields":[{ <strong>//所有的字段列表</strong>
+            "name":"id",
+            "type":{
+              "name":"String",
+              "kind":"NON_NULL"
+            }
+          },{
+            "name":"name",
+            "type":{
+              "name":"String",
+              "kind":"NON_NULL"
+            }
+          },{
+            "name":"gender",
+            "type":{
+              "name":"String",
+              "kind":"NON_NULL"
+            }
+          },{
+            "name":"age",
+            "type":{
+              "name":"Int",
+              "kind":"NON_NULL"
+            }
+          }
+          <strong>//如果有方法也会在这里列出</strong>
+        ]
+      }
+    }}
+    </pre>
+    <ul>
+      <li>内省在需要确认对象的类型和字段列表和类型的时候很有用</li>
+    </ul>
   </div>
 </template>
 
